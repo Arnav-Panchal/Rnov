311b1ff836a58b7e35afb49e92c2bb38ea06da58
Arnav Panchal <arnavpanchal27@gmail.com>
2024-07-20 17:55:09 +0530

    alignment added

--- components/Rbutton.js
+++ components/Rbutton.js
@@ -1,17 +1,18 @@
-// components/AnimatedResumeButton.js
-
+// AnimatedButton.js
+"use client"
 import React from 'react';
+import Link from 'next/link';
 
-const AnimatedResumeButton = () => {
+const AnimatedButton = () => {
   return (
-    <div className="flex justify-center items-center ">
-      <button className="bg-blue-500 text-white py-3 px-6 rounded-lg text-lg transition-transform duration-300 perspective-1000 transform-style-3d hover:rotate-x-10 hover:rotate-y-10 relative overflow-hidden">
-        <span className="block transition-transform duration-300 transform translate-z-0 hover:translate-z-2">
-          RESUME
-        </span>
-      </button>
+    <div className="flex justify-center items-center h-screen">
+      <Link href="Resume.pdf">
+        <div className="transition-colors duration-300 ease-in-out bg-blue-500 text-white border-2 border-transparent rounded-lg py-3 px-6 text-lg cursor-pointer hover:bg-red-500 hover:text-gray-100 hover:border-red-500">
+          Resume
+        </div>
+      </Link>
     </div>
   );
 };
 
-export default AnimatedResumeButton;
+export default AnimatedButton;
