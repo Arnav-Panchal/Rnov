1ef8e343478b64b541d3f9cd46a173a115d79623
Arnav Panchal <arnavpanchal27@gmail.com>
2024-07-22 01:05:37 +0530

    end day changes

--- components/Navr.js
+++ components/Navr.js
@@ -25,21 +25,12 @@ export default function Header() {
     <div>
       <main className={oswald.className}>
         <section className="min-h-screen relative border-b border-yourColor pb-4">
-          <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <Image src={AP} className="h-20 w-20 cursor-pointer" alt="Logo" />
-          </nav>
-          <div className="text-center p-10 py-10" style={customFontStyle}>
-            <h2 className={`${oswald.className} text-4xl sm:text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl hover:scale-105 transition-transform duration-300`}>
+        <h2 className={`${oswald.className} ml-10 mt-6 text-2xl sm:text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl hover:scale-105 transition-transform duration-300`}>
               Arnav Panchal
             </h2>
-            <div className="text-xl py-10 dark:text-white sm:text-2xl md:text-3xl">
-              {/* Additional text or elements can be added here if needed */}
-            </div>
-            <p className="text-sm sm:text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              Freelancer providing services for programming and design content needs. Join me down below and lets get cracking!
-            </p>
-            <div className="text-4xl sm:text-5xl flex justify-center gap-4 sm:gap-12 md:gap-16 py-3 text-gray-600 dark:text-gray-400">
-              <IconContext.Provider value={{ size: '1.3em' }}>
+          <div className="text-center flex p-10 py-10" style={customFontStyle}>
+          <div className="text-4xl sm:text-5xl flex flex-col justify-items-start gap-4 sm:gap-12 md:gap-12 py-3 text-gray-600 dark:text-gray-400">
+              <IconContext.Provider value={{ size: '0.8em' }}>
                 <a href="https://twitter.com/ArnavPanchal9" target="_blank" rel="noopener noreferrer">
                   <AiFillTwitterCircle />
                 </a>
@@ -54,6 +45,18 @@ export default function Header() {
                 </a>
               </IconContext.Provider>
             </div>
+            <div >
+            <h2 className={`${oswald.className} text-4xl sm:text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl hover:scale-105 transition-transform duration-300`}>
+              Arnav Panchal
+            </h2>
+            <div className="text-xl py-10 dark:text-white sm:text-2xl md:text-3xl">
+              Additional text or elements can be added here if needed
+            </div>
+            <p className="text-sm sm:text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+              Freelancer providing services for programming and design content needs. Join me down below and lets get cracking!
+            </p>
+            </div>
+            
           </div>
         </section>
       </main>
